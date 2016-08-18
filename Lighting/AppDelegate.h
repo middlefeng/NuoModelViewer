375422d5f15e487c11e6bee69076e5ae9bf6ac92
middleware <middle.fengdong@gmail.com>
2016-08-17 23:36:57 -0700

    Initial Commit

---
+++ Lighting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Lighting
+//
+//  Created by middleware on 8/17/16.
+//  Copyright © 2016 middleware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
