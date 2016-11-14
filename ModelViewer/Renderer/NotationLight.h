ac2a4ccd6422e53d937a4988e94f4300039b27c4
middleware <middle.fengdong@gmail.com>
2016-11-14 14:46:37 -0800

    New class for the lighting arrow indicator.

---
+++ ModelViewer/Renderer/NotationLight.h
@@ -0,0 +1,39 @@
+//
+//  NotationLight.h
+//  ModelViewer
+//
+//  Created by middleware on 11/13/16.
+//  Copyright © 2016 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Metal/Metal.h>
+#import <simd/simd.h>
+
+
+@class NuoMeshBox;
+
+
+@interface NotationLight : NSObject
+
+
+@property (nonatomic, assign) float rotateX;
+@property (nonatomic, assign) float rotateY;
+
+@property (nonatomic, assign) matrix_float4x4 viewMatrix;
+@property (nonatomic, assign) matrix_float4x4 projMatrix;
+
+
+@property (nonatomic, readonly) NSInteger bufferIndex;
+
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device;
+
+- (void)drawWithRenderPass:(id<MTLRenderCommandEncoder>)renderPass;
+- (void)drawablePresented;
+
+
+- (NuoMeshBox*)boundingBox;
+
+
+@end
