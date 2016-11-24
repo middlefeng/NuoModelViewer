ef745b7feafe32dfd06ebaa868a5866d63a7dd15
middleware <middle.fengdong@gmail.com>
2016-11-23 18:12:33 -0800

    More comment.

--- ModelViewer/ViewerFoundation/Loader/NuoModelLoader.h
+++ ModelViewer/ViewerFoundation/Loader/NuoModelLoader.h
@@ -17,6 +17,12 @@
 @interface NuoModelLoader : NSObject
 
 - (void)loadModel:(NSString*)path;
+
+
+/**
+ *  Create a renderable GPU mesh. A GPU mesh consists of the continuous buffer where the vertex data
+ *  is stored, the associated textures, and the associated pipeline state used for rendering.
+ */
 - (NSArray<NuoMesh*>*)createMeshsWithOptions:(NuoMeshOption*)loadOption
                                   withDevice:(id<MTLDevice>)device;
 
