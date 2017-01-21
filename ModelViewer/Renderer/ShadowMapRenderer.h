4590232a1abfd72589ad8e7c059368a2db966225
middleware <middle.fengdong@gmail.com>
2017-01-21 09:18:18 -0800

    Comments to the shadow map class.

--- ModelViewer/Renderer/ShadowMapRenderer.h
+++ ModelViewer/Renderer/ShadowMapRenderer.h
@@ -21,13 +21,23 @@
 
 @interface ShadowMapRenderer : NuoRenderPass
 
-
+/**
+ *  The light source for which the shadow map is generated for.
+ */
 @property (nonatomic, weak) LightSource* lightSource;
-@property (nonatomic, weak) NSArray<NuoMesh*>* mesh;
 
+/**
+ *  Scene information, mostly passed from the model renderer, hence the
+ *  weak renference. meshMaxSpan is derivable but cached for performance,
+ *  which is cached in the model renderer and passed over to here.
+ */
 @property (nonatomic, assign) float meshMaxSpan;
+@property (nonatomic, weak) NSArray<NuoMesh*>* mesh;
 @property (nonatomic, assign) matrix_float4x4 modelMatrix;
 
+/**
+ *  Shadow map, the depth texture from the view point of the light source.
+ */
 @property (nonatomic, strong) NuoShadowMapTarget* shadowMap;
 
 
