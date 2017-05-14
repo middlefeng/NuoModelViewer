158cd46464a8165a5b1363abc340862487b11e50
middleware <middle.fengdong@gmail.com>
2017-05-14 13:02:25 -0700

    Render context umbrella.

---
+++ ModelViewer/ViewerFoundation/NuoRenderContext.h
@@ -0,0 +1,13 @@
+//
+//  RenderContext.h
+//  ModelViewer
+//
+//  Created by middleware on 5/14/17.
+//  Copyright © 2017 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NuoRenderContext : NSObject
+
+@end
---
+++ ModelViewer/ViewerFoundation/NuoRenderContext.m
@@ -0,0 +1,13 @@
+//
+//  RenderContext.m
+//  ModelViewer
+//
+//  Created by middleware on 5/14/17.
+//  Copyright © 2017 middleware. All rights reserved.
+//
+
+#import "NuoRenderContext.h"
+
+@implementation NuoRenderContext
+
+@end
