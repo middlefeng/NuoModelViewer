5a751d0b08d27b7dbd76e028be56bc08d186746d
Dong Feng <middle.fengdong@gmail.com>
2017-10-26 06:51:52 -0700

    Frame rate view.

---
+++ ModelViewer/UserInterface/FrameRateView.h
@@ -0,0 +1,16 @@
+//
+//  FrameRateView.h
+//  ModelViewer
+//
+//  Created by Dong on 10/25/17.
+//  Copyright © 2017 middleware. All rights reserved.
+//
+
+#import "NuoRoundedView.h"
+
+
+@interface FrameRateView : NuoRoundedView
+
+- (void)showFrameRate:(float)frameRate;
+
+@end
