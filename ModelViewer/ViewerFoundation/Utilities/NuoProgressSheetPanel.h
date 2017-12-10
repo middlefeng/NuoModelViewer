e24f99ca61d3c2fbb6429de43adb7e98b1e9f26a
Dong Feng <middle.fengdong@gmail.com>
2017-12-10 08:47:00 -0800

    Progress indicator panel.

---
+++ ModelViewer/ViewerFoundation/Utilities/NuoProgressSheetPanel.h
@@ -0,0 +1,17 @@
+//
+//  NuoProgressSheetPanel.h
+//  ModelViewer
+//
+//  Created by Dong on 12/10/17.
+//  Copyright © 2017 middleware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+
+@interface NuoProgressSheetPanel : NSPanel
+
+@property (nonatomic, assign) float progress;
+
+@end
---
+++ ModelViewer/ViewerFoundation/Utilities/NuoProgressSheetPanel.m
@@ -0,0 +1,53 @@
+//
+//  NuoProgressSheetPanel.m
+//  ModelViewer
+//
+//  Created by Dong on 12/10/17.
+//  Copyright © 2017 middleware. All rights reserved.
+//
+
+#import "NuoProgressSheetPanel.h"
+
+
+
+@implementation NuoProgressSheetPanel
+{
+    NSProgressIndicator* _indicator;
+}
+
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+        [self initControls];
+    return self;
+}
+
+
+- (void)initControls
+{
+    _indicator = [NSProgressIndicator new];
+    _indicator.indeterminate = NO;
+    [self.contentView addSubview:_indicator];
+    
+    CGRect panelSize = NSMakeRect(0, 0, 340, 120);
+    CGRect progressSize = panelSize;
+    
+    progressSize.size.width -= 50.0;
+    progressSize.size.height = 10;
+    progressSize.origin.x = (panelSize.size.width - progressSize.size.width) / 2.0;
+    progressSize.origin.y = (panelSize.size.height - progressSize.size.height) / 2.0 - 20;
+    
+    [self setFrame:panelSize display:YES];
+    [_indicator setFrame:progressSize];
+}
+
+
+- (void)setProgress:(float)progress
+{
+    [_indicator setDoubleValue:progress];
+}
+
+
+@end
