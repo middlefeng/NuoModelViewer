8750842d83dab2f1b80661237ef4e05e34adf549
Dong Feng <middle.fengdong@gmail.com>
2018-01-28 16:12:53 -0800

    Export/Load of the view transform.

--- ModelViewer/Renderer/ModelViewerRenderer.mm
+++ ModelViewer/Renderer/ModelViewerRenderer.mm
@@ -388,6 +388,12 @@
     }
     
     {
+        exporter.StartEntry("viewMatrix");
+        exporter.SetMatrix(_viewTrans);
+        exporter.EndEntry(true);
+    }
+    
+    {
         exporter.StartEntry("boards");
         exporter.StartTable();
         
@@ -589,6 +595,11 @@
     [lua getField:@"translationMatrix" fromTable:-1];
     if (![lua isNil:-1])
         [_mainModelMesh setTransformTranslate:[lua getMatrixFromTable:-1]];
+    [lua removeField];
+    
+    [lua getField:@"viewMatrix" fromTable:-1];
+    if (![lua isNil:-1])
+        _viewTrans = [lua getMatrixFromTable:-1];
     [lua removeField];
     
     [lua getField:@"view" fromTable:-1];
