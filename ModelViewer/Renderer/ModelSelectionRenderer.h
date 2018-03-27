f31af30df71f99548dd8e3acc08b31a14f71fbec
Dong Feng <middle.fengdong@gmail.com>
2018-03-27 09:28:40 -0700

    Selection indicator renderer.

---
+++ ModelViewer/Renderer/ModelSelectionRenderer.h
@@ -0,0 +1,13 @@
+//
+//  ModelSelectionRenderer.h
+//  ModelViewer
+//
+//  Created by Dong on 3/27/18.
+//  Copyright © 2018 middleware. All rights reserved.
+//
+
+#import "NuoRenderPipelinePass.h"
+
+@interface ModelSelectionRenderer : NuoRenderPipelinePass
+
+@end
---
+++ ModelViewer/Renderer/ModelSelectionRenderer.m
@@ -0,0 +1,13 @@
+//
+//  ModelSelectionRenderer.m
+//  ModelViewer
+//
+//  Created by Dong on 3/27/18.
+//  Copyright © 2018 middleware. All rights reserved.
+//
+
+#import "ModelSelectionRenderer.h"
+
+@implementation ModelSelectionRenderer
+
+@end
