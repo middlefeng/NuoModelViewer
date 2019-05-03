e903ca39c90c68ae17703ceb4d3e0940b0d598be
Dong Feng <middle.fengdong@gmail.com>
2019-05-03 08:45:16 -0700

    Render pass encoder.

---
+++ ModelViewer/ViewerFoundation/NuoRenderPassEncoder.h
@@ -0,0 +1,17 @@
+//
+//  NuoRenderPassEncoder.h
+//  ModelViewer
+//
+//  Created by Dong on 5/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface NuoRenderPassEncoder : NSObject
+
+@end
+
+
---
+++ ModelViewer/ViewerFoundation/NuoRenderPassEncoder.mm
@@ -0,0 +1,13 @@
+//
+//  NuoRenderPassEncoder.m
+//  ModelViewer
+//
+//  Created by Dong on 5/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "NuoRenderPassEncoder.h"
+
+@implementation NuoRenderPassEncoder
+
+@end
