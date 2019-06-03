eac48453b5f59b3b36b40dd59cc1e24cef1f9792
Dong Feng <middle.fengdong@gmail.com>
2019-06-03 09:19:47 -0700

    Class skeleton for the path tracing random unit.

---
+++ ModelViewer/ViewerFoundation/RayTracing/NuoRayTracingRandom.cpp
@@ -0,0 +1,9 @@
+//
+//  NuoRayTracingRandom.cpp
+//  ModelViewer
+//
+//  Created by Dong on 6/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#include "NuoRayTracingRandom.h"
---
+++ ModelViewer/ViewerFoundation/RayTracing/NuoRayTracingRandom.h
@@ -0,0 +1,16 @@
+//
+//  NuoRayTracingRandom.hpp
+//  ModelViewer
+//
+//  Created by Dong on 6/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#ifndef NuoRayTracingRandom_hpp
+#define NuoRayTracingRandom_hpp
+
+
+
+
+
+#endif /* NuoRayTracingRandom_hpp */
