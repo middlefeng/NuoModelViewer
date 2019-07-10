52c263a11336271bc771a88ea7dca06b7ea4b7eb
Dong Feng <middle.fengdong@gmail.com>
2019-07-10 07:44:15 -0700

    Argument buffer class.

---
+++ ModelViewer/ViewerFoundation/Render/NuoArgumentBuffer.h
@@ -0,0 +1,17 @@
+//
+//  NuoArgumentBuffer.h
+//  ModelViewer
+//
+//  Created by Dong on 7/10/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface NuoArgumentBuffer : NSObject
+
+@end
+
+
---
+++ ModelViewer/ViewerFoundation/Render/NuoArgumentBuffer.mm
@@ -0,0 +1,13 @@
+//
+//  NuoArgumentBuffer.m
+//  ModelViewer
+//
+//  Created by Dong on 7/10/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "NuoArgumentBuffer.h"
+
+@implementation NuoArgumentBuffer
+
+@end
