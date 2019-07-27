c4b6a83b8531bf7198463c6a44b8c8fe371f736f
Dong Feng <middle.fengdong@gmail.com>
2019-07-27 08:13:26 -0700

    Scene parameter provider.

--- ModelViewer/Renderer/ModelSceneParameters.h
+++ ModelViewer/Renderer/ModelSceneParameters.h
@@ -6,11 +6,52 @@
 //  Copyright © 2019 middleware. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "NuoMeshSceneRenderPass.h"
+#import "NuoUniforms.h"
 
 
 
-@interface ModelSceneParameters : NSObject
+@protocol NuoRenderInFlight;
+@class NuoMeshSceneRoot;
+@class NuoLightSource;
+
+
+
+@protocol ModelShadowMapProvider <NSObject>
+
+- (id<MTLTexture>)depthMap;
+- (id<MTLTexture>)shadowMap:(uint)index withMask:(NuoSceneMask)mask;
+
+@end
+
+
+
+@interface ModelSceneParameters : NSObject < NuoMeshSceneParametersProvider >
+
+
+@property (assign, nonatomic) BOOL cullEnabled;
+@property (weak, nonatomic) NuoMeshSceneRoot* sceneRoot;
+@property (weak, nonatomic) NSArray<NuoLightSource*>* lights;
+
+@property (assign, nonatomic) float ambient;
+
+@property (assign, nonatomic) float fieldOfView;
+@property (assign, nonatomic) CGSize drawableSize;
+@property (assign, nonatomic) NuoMatrixFloat44 viewMatrix;
+@property (readonly, nonatomic) NuoMatrixFloat44 projection;
+
+
+@property (weak, nonatomic) id<ModelShadowMapProvider> shadowMap;
+
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device;
+
+
+- (void)updateLightCastWithInFlight:(id<NuoRenderInFlight>)inFlight
+                        withContent:(NuoLightVertexUniforms*)content;
+
+- (void)updateUniforms:(NuoCommandBuffer*)commandBuffer;
+
 
 @end
 
--- ModelViewer/Renderer/ModelSceneParameters.mm
+++ ModelViewer/Renderer/ModelSceneParameters.mm
@@ -8,6 +8,145 @@
 
 #import "ModelSceneParameters.h"
 
+#import "NuoTypes.h"
+#import "NuoCommandBuffer.h"
+#import "NuoBufferSwapChain.h"
+#import "NuoLightSource.h"
+
+#import "NuoMeshSceneRoot.h"
+
+
+
+
 @implementation ModelSceneParameters
+{
+    NuoBufferSwapChain* _transUniformBuffers;
+    NuoBufferSwapChain* _lightingUniformBuffers;
+    NuoBufferSwapChain* _lightCastBuffers;
+    id<MTLBuffer> _modelCharacterUnfiromBuffer;
+}
+
+
+- (instancetype)initWithDevice:(id<MTLDevice>)device
+{
+    if (self = [super init])
+    {
+        _transUniformBuffers = [[NuoBufferSwapChain alloc] initWithDevice:device
+                                                           WithBufferSize:sizeof(NuoUniforms)
+                                                              withOptions:MTLResourceStorageModeManaged
+                                                            withChainSize:kInFlightBufferCount];
+        
+        _lightingUniformBuffers = [[NuoBufferSwapChain alloc] initWithDevice:device
+                                                              WithBufferSize:sizeof(NuoLightUniforms)
+                                                                 withOptions:MTLResourceStorageModeManaged
+                                                               withChainSize:kInFlightBufferCount];
+        
+        _lightCastBuffers = [[NuoBufferSwapChain alloc] initWithDevice:device
+                                                        WithBufferSize:sizeof(NuoLightVertexUniforms)
+                                                           withOptions:MTLResourceStorageModeManaged
+                                                         withChainSize:kInFlightBufferCount];
+        
+        NuoModelCharacterUniforms modelCharacter;
+        modelCharacter.opacity = 1.0f;
+        _modelCharacterUnfiromBuffer = [device newBufferWithLength:sizeof(NuoModelCharacterUniforms)
+                                                           options:MTLResourceOptionCPUCacheModeDefault];
+        
+        memcpy([_modelCharacterUnfiromBuffer contents], &modelCharacter, sizeof(NuoModelCharacterUniforms));
+    }
+    
+    return self;
+}
+
+
+- (void)updateUniforms:(NuoCommandBuffer*)commandBuffer
+{
+    const NuoMatrixFloat44& viewTrans = _viewMatrix;
+    
+    const CGSize& drawableSize = _drawableSize;
+    const float aspect = drawableSize.width / drawableSize.height;
+    
+    // bounding box transform and determining the near/far
+    //
+    NuoBounds bounds = [_sceneRoot worldBounds:viewTrans].boundingBox;
+    
+    float near = -bounds._center.z() - bounds._span.z() / 2.0 + 0.01;
+    float far = near + bounds._span.z() + 0.02;
+    near = std::max<float>(0.001, near);
+    far = std::max<float>(near + 0.001, far);
+    
+    _projection = NuoMatrixPerspective(aspect, _fieldOfView, near, far);
+    
+    NuoUniforms uniforms;
+    uniforms.viewMatrix = viewTrans._m;
+    uniforms.viewMatrixInverse = viewTrans.Inverse()._m;
+    uniforms.viewProjectionMatrix = (_projection * viewTrans)._m;
+    
+    [_transUniformBuffers updateBufferWithInFlight:commandBuffer withContent:&uniforms];
+    
+    NuoLightUniforms lighting;
+    lighting.ambientDensity = _ambient;
+    for (unsigned int i = 0; i < 4; ++i)
+    {
+        const NuoMatrixFloat44 rotationMatrix = NuoMatrixRotation(_lights[i].lightingRotationX,
+                                                                  _lights[i].lightingRotationY);
+        
+        const NuoVectorFloat4 lightVector(rotationMatrix * NuoVectorFloat4(0, 0, 1, 0));
+        lighting.lightParams[i].direction = lightVector._vector;
+        lighting.lightParams[i].density = _lights[i].lightingDensity;
+        lighting.lightParams[i].specular = _lights[i].lightingSpecular;
+        
+        if (i < 2)
+        {
+            lighting.shadowParams[i].soften = _lights[i].shadowSoften;
+            lighting.shadowParams[i].bias = _lights[i].shadowBias;
+            lighting.shadowParams[i].occluderRadius = _lights[i].shadowOccluderRadius;
+        }
+    }
+    
+    [_lightingUniformBuffers updateBufferWithInFlight:commandBuffer withContent:&lighting];
+}
+
+
+- (void)updateLightCastWithInFlight:(id<NuoRenderInFlight>)inFlight
+                        withContent:(NuoLightVertexUniforms*)content
+{
+    [_lightCastBuffers updateBufferWithInFlight:inFlight withContent:content];
+}
+
+
+- (BOOL)cullEnabled
+{ 
+    return _cullEnabled;
+}
+
+- (id<MTLTexture>)depthMap
+{
+    return [_shadowMap depthMap];
+}
+
+- (NuoBufferSwapChain *)lightCastBuffers
+{
+    return _lightCastBuffers;
+}
+
+- (NuoBufferSwapChain *)lightingUniformBuffers
+{
+    return _lightingUniformBuffers;
+}
+
+- (id<MTLBuffer>)modelCharacterUnfiromBuffer
+{
+    return _modelCharacterUnfiromBuffer;
+}
+
+- (id<MTLTexture>)shadowMap:(uint)index withMask:(NuoSceneMask)mask
+{
+    return [_shadowMap shadowMap:index withMask:mask];
+}
+
+- (NuoBufferSwapChain *)transUniformBuffers
+{
+    return _transUniformBuffers;
+}
 
 @end
