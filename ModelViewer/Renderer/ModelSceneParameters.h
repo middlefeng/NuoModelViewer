eb2de1cb2c1b9fbbb148728c4eeb8e71615b2e14
Dong Feng <middle.fengdong@gmail.com>
2019-07-26 17:18:08 -0700

    Separated files for the parameter provider.

---
+++ ModelViewer/Renderer/ModelSceneParameters.h
@@ -0,0 +1,17 @@
+//
+//  ModelSceneParameters.h
+//  ModelViewer
+//
+//  Created by Dong on 7/26/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface ModelSceneParameters : NSObject
+
+@end
+
+
---
+++ ModelViewer/Renderer/ModelSceneParameters.mm
@@ -0,0 +1,13 @@
+//
+//  ModelSceneParameters.m
+//  ModelViewer
+//
+//  Created by Dong on 7/26/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "ModelSceneParameters.h"
+
+@implementation ModelSceneParameters
+
+@end
