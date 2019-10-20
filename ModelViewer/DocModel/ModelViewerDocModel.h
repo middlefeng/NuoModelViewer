647268247b7f3e75a573393d4df4105f3501f9dd
Dong Feng <middle.fengdong@gmail.com>
2019-10-20 07:22:41 -0700

    Centralized app model.

---
+++ ModelViewer/DocModel/ModelViewerDocModel.h
@@ -0,0 +1,17 @@
+//
+//  ModelViewerDocModel.h
+//  ModelViewer
+//
+//  Created by Dong on 10/20/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface ModelViewerDocModel : NSObject
+
+@end
+
+
---
+++ ModelViewer/DocModel/ModelViewerDocModel.mm
@@ -0,0 +1,13 @@
+//
+//  ModelViewerDocModel.m
+//  ModelViewer
+//
+//  Created by Dong on 10/20/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "ModelViewerDocModel.h"
+
+@implementation ModelViewerDocModel
+
+@end
