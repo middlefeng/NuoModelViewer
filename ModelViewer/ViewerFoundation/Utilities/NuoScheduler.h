ac74b30297ce2fceb804b211d2094eb3d6fb9f35
Dong Feng <middle.fengdong@gmail.com>
2019-12-03 13:59:46 -0800

    Scheduler class.

---
+++ ModelViewer/ViewerFoundation/Utilities/NuoScheduler.h
@@ -0,0 +1,17 @@
+//
+//  NuoScheduler.h
+//  ModelViewer
+//
+//  Created by Dong on 12/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface NuoScheduler : NSObject
+
+@end
+
+
---
+++ ModelViewer/ViewerFoundation/Utilities/NuoScheduler.mm
@@ -0,0 +1,13 @@
+//
+//  NuoScheduler.m
+//  ModelViewer
+//
+//  Created by Dong on 12/3/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "NuoScheduler.h"
+
+@implementation NuoScheduler
+
+@end
