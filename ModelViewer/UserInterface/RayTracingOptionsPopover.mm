9bd6a1896d31484eb672c0c8459cec7132575db5
Dong Feng <middle.fengdong@gmail.com>
2019-12-08 07:07:51 -0800

    Ray tracing options view controller.

---
+++ ModelViewer/UserInterface/RayTracingOptionsPopover.mm
@@ -0,0 +1,13 @@
+//
+//  RayTracingOptionsPopover.m
+//  ModelViewer
+//
+//  Created by Dong on 12/8/19.
+//  Copyright © 2019 middleware. All rights reserved.
+//
+
+#import "RayTracingOptionsPopover.h"
+
+@implementation RayTracingOptionsPopover
+
+@end
