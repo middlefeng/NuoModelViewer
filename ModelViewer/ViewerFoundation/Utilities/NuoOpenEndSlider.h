88131ab9198231f82b66a97fa246d931de3b1132
Dong Feng <middle.fengdong@gmail.com>
2020-01-17 08:30:48 -0800

    Open end slider control.

---
+++ ModelViewer/ViewerFoundation/Utilities/NuoOpenEndSlider.h
@@ -0,0 +1,17 @@
+//
+//  NuoOpenEndSlider.h
+//  ModelViewer
+//
+//  Created by Dong on 1/17/20.
+//  Copyright © 2020 middleware. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+
+@interface NuoOpenEndSlider : NSView
+
+@end
+
+
---
+++ ModelViewer/ViewerFoundation/Utilities/NuoOpenEndSlider.m
@@ -0,0 +1,15 @@
+//
+//  NuoOpenEndSlider.m
+//  ModelViewer
+//
+//  Created by Dong on 1/17/20.
+//  Copyright © 2020 middleware. All rights reserved.
+//
+
+#import "NuoOpenEndSlider.h"
+
+@implementation NuoOpenEndSlider
+
+
+
+@end
